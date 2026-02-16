64c746d97b36dc2de84330603231186707a6c8eb
Missinggirl201 <TY202501263@WMSU.EDU.PH>
2026-02-16 18:57:11 +0800

    Add files via upload

---
+++ ManuelCC102Lab4_2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m = 12;
+    double sales[m];
+    double t = 0, ave;
+    int hm = 0, lm = 0;
+
+    cout << "Enter the sales for 12 months: " << endl;
+
+    for (int i = 0; i < m; i++) {
+        cout << "Month " << i + 1 << ": ";
+        cin >> sales[i];
+        t += sales[i];
+
+        if (sales[i] > sales[hm]) {
+            hm = i;
+        }
+
+        if (sales[i] < sales[lm]) {
+            lm = i;
+        }
+    }
+
+    ave = t / m;
+
+    
+    cout << "-------------------------------------\n";
+    cout << "Total Sales: " << t << endl;
+    cout << "-------------------------------------\n";
+    cout << "Average Monthly Sales: " << ave << endl;
+    cout << "-------------------------------------\n";
+    cout << "Highest Sales: Month " << hm + 1 
+         << " (" << sales[hm] << ")" << endl;
+    cout << "-------------------------------------\n";
+    cout << "Lowest Sales: Month " << lm + 1
+         << " (" << sales[lm] << ")" << endl;
+         
+    cout << "-------------------------------------\n";
+
+    return 0;
+}
